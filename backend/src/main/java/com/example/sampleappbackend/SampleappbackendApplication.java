9c7d1f910c9f87d89cfe595b9f2197845cb66d0c
Kentkn192837 <agriculture5012@gmail.com>
2025-06-08 04:19:55 +0000

    first commit

---
+++ backend/src/main/java/com/example/sampleappbackend/SampleappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleappbackendApplication.class, args);
+	}
+
+}
